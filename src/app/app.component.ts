0d7dc474e1254f59effa2d5e264fec0c047c3492
brunapsdonato <103545588+brunapsdonato@users.noreply.github.com>
2024-12-10 16:48:45 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PrimeiroAngular';
+  title = 'Bruna Priscila Sousa Donato';
 }
